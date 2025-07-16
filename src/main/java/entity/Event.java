69f58e02e5d9ef652b39e6ea25b2a2cd067a3da5
Janya Singh <singhjanya48@gmail.com>
2025-07-16 14:14:49 -0400

    Added initial events class

--- src/main/java/entity/Event.java
+++ src/main/java/entity/Event.java
@@ -1,5 +1,49 @@
 package entity;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Event {
-    // TODO: add instance variables and methods
+    private String name;
+    private String description;
+    private String location;
+    private LocalDate date;
+    private ArrayList<Account> participants;
+    private ArrayList<String> foodPreferences;
+
+    public Event(String name, String description, String location, LocalDate date, ArrayList<Account> participants, ArrayList<String> foodPreferences) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.date = date;
+        this.participants = participants;
+        this.foodPreferences = foodPreferences;
+    }
+    public String getName() {return name;}
+
+    public String getDescription() {return description;}
+
+    public String getLocation() {return location;}
+
+    public LocalDate getDate() {return date;}
+
+    public ArrayList<Account> getParticipants() {return participants;}
+
+    public ArrayList<String> getFoodPreferences() {return foodPreferences;}
+
+    public void addParticipant(Account account) {participants.add(account);}
+
+    public void removeParticipant(Account account) {participants.remove(account);}
+
+    public void addFoodPreference(String preference) {foodPreferences.add(preference);}
+
+    public void removeFoodPreference(String preference) {foodPreferences.remove(preference);}
+
+    public void editDate(LocalDate newdate) {this.date = newdate;}
+
+    public void editDescription(String newdescription) {this.description = newdescription;}
+
+    public void editName(String newname) {this.name = newname;}
+
+    public void editLocation(String newlocation) {this.location = newlocation;}
 }
