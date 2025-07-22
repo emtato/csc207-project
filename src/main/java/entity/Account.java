50470939df30598b4accf40c058a2c6341ddbd29
S <s.saghatelyan@outlook.com>
2025-07-22 04:05:36 -0400

    Progress in the view code, unfinished

--- src/main/java/entity/Account.java
+++ src/main/java/entity/Account.java
@@ -3,75 +3,161 @@ package entity;
 import java.util.ArrayList;
 
 /**
- * A simple implementation of the User interface.
+ * An implementation of the User interface.
  */
 public class Account implements User {
-
-    private final String name;
-    private final String password;
-
+    private String username;
+    private String password;
+    private String name;
+    private String email;
     private String bio;
-    private final ArrayList<Account> followingAccounts = new ArrayList<>();
-    private final ArrayList<Account> followerAccounts = new ArrayList<>();
-    private final ArrayList<Account> blockedAccounts = new ArrayList<>();
-    private final ArrayList<String> blockedTerms = new ArrayList<>();
-    private final ArrayList<Account> mutedAccounts = new ArrayList<>();
-    private final ArrayList<String> foodPreferences = new ArrayList<>();
+    private ArrayList<Account> followerAccounts;
+    private ArrayList<Account> blockedAccounts;
+    private ArrayList<String> blockedTerms;
+    private ArrayList<Account> mutedAccounts;
+    private ArrayList<String> foodPreferences;
+    private ArrayList<Integer> likesUsernames;
+    private ArrayList<String> followingAccounts;
 
-    public Account(String name, String password) {
-        this.name = name;
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        likesUsernames = new ArrayList<>();
+        followingAccounts = new ArrayList<>();
+        followerAccounts = new ArrayList<>();
+        blockedAccounts = new ArrayList<>();
+        blockedTerms = new ArrayList<>();
+        mutedAccounts = new ArrayList<>();
+        foodPreferences = new ArrayList<>();
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    // Length of bio is less than 200 characters.
-    public String editBio(String bio) {
-        if (bio.length() <= 200) {
-            this.bio = bio;
-        }
-        return this.bio;
-    }
-
-
-    public void toggleFollowing(Account following) {
-        // TODO: add if Account following is a valid username (already created)
-        if (this.followerAccounts.contains(following)) {
-            this.followerAccounts.remove(following);
-        }
-        else {
-            this.followerAccounts.add(following);
-        }
-    }
-
-    // TODO: how to see follower list
-
-
-    @Override
     public String getName() {
         return name;
     }
 
-    @Override
-    public String getPassword() {
-        return password;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getBio() {
         return bio;
     }
 
-    public ArrayList<Account> getFollowingAccounts() {
-        return followingAccounts;
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public ArrayList<Integer> getLikesUsernames() {
+        return likesUsernames;
+    }
+
+    public void setLikesUsernames(ArrayList<Integer> likesUsernames) {
+        this.likesUsernames = likesUsernames;
+    }
+
+    public ArrayList<Account> getFollowerAccounts() {
+        return followerAccounts;
+    }
+
+    public void setFollowerAccounts(ArrayList<Account> followerAccounts) {
+        this.followerAccounts = followerAccounts;
+    }
+
+    public ArrayList<Account> getBlockedAccounts() {
+        return blockedAccounts;
+    }
+
+    public void setBlockedAccounts(ArrayList<Account> blockedAccounts) {
+        this.blockedAccounts = blockedAccounts;
     }
 
     public ArrayList<String> getBlockedTerms() {
         return blockedTerms;
     }
 
+    public void setBlockedTerms(ArrayList<String> blockedTerms) {
+        this.blockedTerms = blockedTerms;
+    }
+
     public ArrayList<Account> getMutedAccounts() {
         return mutedAccounts;
     }
 
+    public void setMutedAccounts(ArrayList<Account> mutedAccounts) {
+        this.mutedAccounts = mutedAccounts;
+    }
+
     public ArrayList<String> getFoodPreferences() {
         return foodPreferences;
+    }
+
+    public void setFoodPreferences(ArrayList<String> foodPreferences) {
+        this.foodPreferences = foodPreferences;
+    }
+
+
+    public boolean liked(Post p) {
+        return likesUsernames.contains(p.getID());
+    }
+
+    public void like(Post p) {
+        likesUsernames.add(p.getID());
+    }
+
+    public void dislike(Post p) {
+        likesUsernames.remove( (Integer) p.getID());
+    }
+
+
+    public void setFriends(ArrayList<Account> friends) {
+        followingAccounts = new ArrayList<>();
+        for (Account account : friends) {
+            followingAccounts.add(account.getUsername());
+        }
+    }
+
+    public void setFollowingAccounts(ArrayList<String> followingAccounts) {
+        this.followingAccounts = followingAccounts;
+    }
+
+    public ArrayList<String> getFollowingAccounts() {
+        return followingAccounts;
+    }
+
+    public boolean isFriend(Account account) {
+        return followingAccounts.contains(account.getUsername());
+    }
+
+    public void addFriend(Account f) {
+        followingAccounts.add(f.getUsername());
+    }
+
+    public void removeFriend(Account f) {
+        followingAccounts.remove(f.getUsername());
     }
 }
