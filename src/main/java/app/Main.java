39f5584702a91b612954876edd1cadc8a204c993
emtato <emiliama2006@gmail.com>
2025-07-26 18:52:50 -0400

    push

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -29,8 +29,7 @@ public class Main {
                 .addChangePasswordUseCase()
                 .addLogoutUseCase()
                 .addNoteUseCase()
-                .addHomePageUseCase()
-                .addAnalyzeRecipesView()
+                .addPostView()
                 .build();
         application.setPreferredSize(new Dimension(1920, 1080));
         application.pack();
