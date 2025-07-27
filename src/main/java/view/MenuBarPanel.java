54391a91699d5060896a81c3a7d090263987ff9b
emtato <emiliama2006@gmail.com>
2025-07-26 20:18:31 -0400

    restoring kate (kat3sjy)'s MenuBarPanel file (fix from bad push)

---
+++ src/main/java/view/MenuBarPanel.java
@@ -0,0 +1,34 @@
+// src/main/java/view/MenuBarPanel.java
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import interface_adapter.ViewManagerModel;
+
+public class MenuBarPanel extends JPanel {
+    public MenuBarPanel(ViewManagerModel viewManagerModel) {
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        JButton homeButton = new JButton("Home");
+        JButton clubsButton = new JButton("Clubs");
+        JButton settingsButton = new JButton("Settings");
+        JButton eventsButton = new JButton("Events");
+        JButton recipeButton = new JButton("Recipes");
+
+
+
+        homeButton.addActionListener(e -> viewManagerModel.setState("homepage view"));
+        clubsButton.addActionListener(e -> viewManagerModel.setState("club view"));
+        settingsButton.addActionListener(e -> viewManagerModel.setState("settings"));
+        eventsButton.addActionListener(e -> viewManagerModel.setState("explore events view"));
+        recipeButton.addActionListener(e -> viewManagerModel.setState("recipe view"));
+
+
+        add(homeButton);
+        add(clubsButton);
+        add(settingsButton);
+        add(eventsButton);
+        add(recipeButton);
+
+
+    }
+}
