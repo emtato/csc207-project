56f27e04be3860b62e0e9b8ff750c2482d1d2a6a
emtato <emiliama2006@gmail.com>
2025-07-28 19:52:06 -0400

    2 post wide feed and making feed more compact

--- src/main/java/view/PostPanel.java
+++ src/main/java/view/PostPanel.java
@@ -35,19 +35,16 @@ public class PostPanel extends JPanel {
 
     // right
     private RoundedButton likeButton = new RoundedButton("Like");
-    private final RoundedButton analyzeButton = new RoundedButton("Analyze");
     private final RoundedButton saveButton = new RoundedButton("Add to list");
     private final RoundedButton shareButton = new RoundedButton("Share");
-    private final RoundedButton commentButton = new RoundedButton("coment");
+    private final RoundedButton viewFullPost = new RoundedButton("view full post");
 
     private final JLabel title;
     private final JLabel subtitle;
 
     private boolean liked;
     private JPanel centerPanel;
-    private JScrollPane scrollPane;
     private JPanel rightPanel;
-    private boolean xPresent;
 
     public PostPanel(ViewManagerModel viewManagerModel, Post post, int postWidth, int postHeight) {
         this.viewManagerModel = viewManagerModel;
@@ -90,16 +87,9 @@ public class PostPanel extends JPanel {
             System.out.println("cry");
         }
 
-        scrollPane = new JScrollPane(postText);
 
-
-        JTextArea comments = new JTextArea();
-        scrollPane.add(comments);
-        scrollPane.setPreferredSize(new Dimension(postWidth, postHeight));
-        centerPanel.add(scrollPane);
+        centerPanel.add(postText);
         centerPanel.add(Box.createRigidArea(new Dimension(10, 10)));
-        comments.setBackground(Color.PINK);
-        comments.setOpaque(true);
 
         // right
         ArrayList<JButton> rightButtons = new ArrayList<>();
@@ -107,12 +97,9 @@ public class PostPanel extends JPanel {
             likeButton.setText("unlike");
         }
         rightButtons.add(likeButton);
-        if (post instanceof Recipe) {
-            rightButtons.add(analyzeButton);
-        }
         rightButtons.add(saveButton);
         rightButtons.add(shareButton);
-        rightButtons.add(commentButton);
+        rightButtons.add(viewFullPost);
         for (JButton button : rightButtons) {
             button.setFont(text);
             button.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -131,7 +118,6 @@ public class PostPanel extends JPanel {
             rightPanel.add(button);
             rightPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         }
-        rightPanel.add(Box.createRigidArea(new Dimension(0, 666)));
 
         mainPanel.add(topPanel, BorderLayout.NORTH);
         mainPanel.add(centerPanel, BorderLayout.CENTER);
@@ -157,40 +143,6 @@ public class PostPanel extends JPanel {
             subtitle.setText(post.getUser().getUsername() + " | " + post.getLikes() + " likes");
 
         }
-        if (e.getSource() == analyzeButton) {
-            System.out.println("hmmmm \uD83E\uDD13");
-            SpoonacularAPI spon = new SpoonacularAPI();
-            this.repice.setRestrictionsMap(spon.callAPI(repice));
-            HashMap<String, String> result = this.repice.getRestrictionsMap();
-            System.out.println(result);
-            String resultDisplay = "";
-            String numers = "";
-            for (String key : result.keySet()) {
-                String loopRes = String.valueOf(result.get(key));
-                if (loopRes.equals("true") || loopRes.equals("false")) {
-                    if (loopRes.equals("true")) {
-                        resultDisplay += "is " + key + " ✅\uD83D\uDE04💪 \n";
-                    }
-                    else {
-                        resultDisplay += "is not " + key + "💔😿😔 \n";
-                    }
-                }
-                else {
-                    if (Double.valueOf(loopRes) > 30) {
-                        numers += key + ": " + loopRes + " \n";
-                    }
-                    else {
-                        numers += key + ": " + loopRes + " \uD83D\uDE25  \n";
-                    }
-                }
-            }
-            JTextArea messageArea = new JTextArea("according to le analysis: \n" + resultDisplay + numers);
-            messageArea.setFont(whimsy);
-            messageArea.setEditable(false);
-            messageArea.setOpaque(false);
-            JOptionPane.showMessageDialog(null, messageArea, "nerd", JOptionPane.INFORMATION_MESSAGE);
-
-        }
         if (e.getSource() == saveButton) {
             JOptionPane.showMessageDialog(null, "havent added that yet cry abt it", "nerd", JOptionPane.INFORMATION_MESSAGE);
 
@@ -200,57 +152,11 @@ public class PostPanel extends JPanel {
             System.out.println("share slop");
             JOptionPane.showMessageDialog(null, "here is the id of ths post share that or something \n" + post.getID(), "nerd", JOptionPane.INFORMATION_MESSAGE);
         }
-        if (e.getSource() == commentButton && !xPresent) {
-            JTextArea commentsArea = new JTextArea(2, 20);
-            scrollPane.setSize(new Dimension(1400, 800)); //YOPPP WORKS
-            centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-            centerPanel.add(commentsArea);
-
-            commentButton.setOpaque(true);
-            commentButton.setText("");
-
-            JPanel subRight = new JPanel(new FlowLayout(FlowLayout.LEFT));
-            RoundedButton xButton = new RoundedButton("x");
-            RoundedButton postButton = new RoundedButton("Post!");
-            subRight.add(postButton);
-            subRight.add(xButton);
-            rightPanel.add(subRight);
-
-            xPresent = true;
-            xButton.addActionListener(
-                    new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            centerPanel.remove(commentsArea);
-                            rightPanel.remove(subRight);
-                            commentButton.setText("comment");
-                            commentButton.setOpaque(false);
-
-//                          centerPanel.revalidate();
-//                          centerPanel.repaint();
-//                          rightPanel.revalidate();
-//                          rightPanel.repaint();
-                            xPresent = false;
-                        }
-                    });
-            postButton.addActionListener(
-                    new ActionListener() {
-                        public void actionPerformed(ActionEvent e) {
-                            String mesage = commentsArea.getText();
-                            //TODO: send comment to whoever deals with this idk
-                            centerPanel.remove(commentsArea);
-                            rightPanel.remove(subRight);
-                            commentButton.setText("comment");
-                            commentButton.setOpaque(false);
-                            xPresent = false;
-                            HashMap<Integer, Comment> map = post.getComments();
-                            //TODO: account user implementation
-                            Comment comment = new Comment(new Account("hi", "bye"), mesage, LocalDateTime.now(), 0);
-                            map.put(comment.getID(), comment);
-                            post.setComments(map);
-                        }
-                    }
-            );
+        if (e.getSource() == viewFullPost) {
+            viewManagerModel.setState("post view");
+            //PostView post = new PostView();
+            //post.displayPost()
+            //TODO: send information on which post has been expanded to postView
         }
 
     }
