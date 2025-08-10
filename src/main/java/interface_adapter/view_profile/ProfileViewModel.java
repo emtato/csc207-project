e7946524c51c2f36ab673d60a6c6e52895ca4ec5
Amanda <77864882+amansdali@users.noreply.github.com>
2025-08-09 23:41:12 -0400

    added the ability to view the profile of other users in the profile view using the view profile use case

--- src/main/java/interface_adapter/view_profile/ProfileViewModel.java
+++ src/main/java/interface_adapter/view_profile/ProfileViewModel.java
@@ -9,18 +9,22 @@ public class ProfileViewModel extends ViewModel<ProfileState> {
 
     public static final String TITLE_LABEL = "Profile Menu";
     public static final int BIO_ROW_NUM = 3;
-    public static final int BIO_COL_NUM = 20;
+    public static final int BIO_COL_NUM = 10;
     public static final String EDIT_PROFILE_BUTTON_LABEL = "Edit Profile";
     public static final String FOLLOWING_BUTTON_LABEL = "Following";
     public static final String FOLLOWERS_BUTTON_LABEL = "Followers";
     public static final int CONTENT_PANEL_WIDTH = 1200;
     public static final int CONTENT_PANEL_HEIGHT = 250;
+    public static final int INFO_PANEL_WIDTH = 1000;
+    public static final int INFO_PANEL_HEIGHT = 300;
     public static final int PFP_WIDTH = 200;
     public static final int PFP_HEIGHT = 200;
     public static final int MAIN_PANEL_WIDTH = 1200;
     public static final int MAIN_PANEL_HEIGHT = 300;
     public static final int POST_WIDTH = 600;
     public static final int POST_HEIGHT = 400;
+    public static final String SEE_MY_PROFILE_BUTTON_LABEL = "View My Profile";
+    public static final String SEE_OTHER_PROFILE_BUTTON_LABEL = "View Different Profile";
 
     public ProfileViewModel() {
         super("profile");
